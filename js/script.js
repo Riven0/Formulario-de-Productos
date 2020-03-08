854c8354eee7706a2178e002810b2268a10fc29f
Riven0 <dantesoo22@gmail.com>
2020-03-07 18:17:13 -0600

    Formulario de productos

---
+++ js/script.js
@@ -0,0 +1,13 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyBdTRZxYr0jpoy8KuMEbl_yMwve7E1S21c",
+    authDomain: "maxapp-85793.firebaseapp.com",
+    databaseURL: "https://maxapp-85793.firebaseio.com",
+    projectId: "maxapp-85793",
+    storageBucket: "maxapp-85793.appspot.com",
+    messagingSenderId: "1044203467991",
+    appId: "1:1044203467991:web:f7adbaa64dae14ad4bec92",
+    measurementId: "G-NS789PQCZ4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
